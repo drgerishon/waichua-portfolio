45d3362b167b351f2b1ff1bba026dcd72b66120b
drgerishon <gerishonkungu@gmail.com>
2022-10-27 09:04:45 +0300

    &apos;

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,38 +5,37 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Main = () => {
   return (
-    <div id='main' className="w-full h-screen text-center">
+    <div id="main" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
             LETS BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            HI, I'm <span className="text-[#5651e5]">Gerishon</span>
+            HI, I&apos;m <span className="text-[#5651e5]">Gerishon</span>
           </h1>
-          <h1 className='"py-4 text-gray-700"'>
-            A full-stack developer
-          </h1>
-          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-            I'm a full-stack web developer speacilaizing in building exceptional
-            digital experiences. Currently, I'm focused on building reasonable front-end
-            applications while learning back-end techonogies.
+          <h1 className='"py-4 text-gray-700"'>A full-stack developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            I&apos;m a full-stack web developer speacilaizing in building
+            exceptional digital experiences. Currently, I&apos;m focused on
+            building reasonable front-end applications while learning back-end
+            techonogies.
           </p>
-          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaLinkedinIn />
+              <FaLinkedinIn />
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaGithub />
+              <FaGithub />
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <AiOutlineMail />
+              <AiOutlineMail />
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaTwitter />
+              <FaTwitter />
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <BsFillPersonLinesFill />
+              <BsFillPersonLinesFill />
             </div>
           </div>
         </div>
